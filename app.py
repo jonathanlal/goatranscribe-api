9a6b8ebffaf004ba1e2206e058f40e81693b3c4c
jonathanlal <jonathanmlaliberte@gmail.com>
2023-04-19 05:46:13 +0200

    try endpoints

--- app.py
+++ app.py
@@ -34,6 +34,8 @@ app = Flask(__name__)
 # access tokens with an Auth0 API audience, excluding the /userinfo endpoint, cannot have private, non-namespaced custom claims
 # https://auth0.com/docs/secure/tokens/json-web-tokens/json-web-token-claims 
 def getUserID(token):
+    if not token:
+        return 'try'
     sub_value = token.get('sub', '')
     return sub_value.replace('|', '')
 
@@ -113,6 +115,47 @@ def file_exists_azure(blob_name):
 def download_file_from_azure(blob_name):
     blob_client = get_blob_client(blob_name)
     return blob_client.download_blob()
+
+
+@app.route("/try/transcribe", methods=["POST"])
+def tryTranscribe():
+    openai.api_key = env.get("OPENAI_API_KEY")
+    blob_name = request.json['entryKey']
+    
+    transcript_file_name = f"transcript/{blob_name}"
+    subtitle_file_name = f"subtitle/{blob_name}"
+    audio_file_name = f"audio/{blob_name}"
+
+    user_id = 'try'
+
+    audio_file = download_file_from_azure(audio_file_name)
+    file_extension = get_blob_client(audio_file_name).get_blob_properties().metadata['fileExtension']
+
+    audio_content = audio_file.content_as_bytes()
+    with tempfile.NamedTemporaryFile(suffix=file_extension, delete=False) as tmp_file:
+        tmp_file.write(audio_content)
+        tmp_file.seek(0)
+        subtitles = transcribe_audio(tmp_file)
+
+    os.remove(tmp_file.name)
+    upload_file_to_azure(subtitle_file_name, subtitles)
+    update_subtitle_file_info(blob_name)
+
+
+    subs = list(srt.parse(subtitles))
+    transcript = extract_text_from_srt(subs)
+    subs_dicts = [subtitle_to_dict(sub) for sub in subs]
+    tokens = word_tokenize(transcript)
+    word_count = len(tokens)
+    metadata = {
+        'wordCount': str(word_count)
+    }
+    upload_file_to_azure(transcript_file_name, transcript, metadata=metadata) #upload text transcript to azure
+    update_transcript_file_info(blob_name)
+
+        
+    return createTranscribeResponse(blob_name, subs_dicts, transcript)
+
 
 @app.route("/api/transcribe", methods=["POST"])
 @require_auth(None)
@@ -265,6 +308,15 @@ def uploadComplete():
     }
     return jsonify(response)
 
+@app.route("/try/uploadComplete", methods=["POST"])
+def tryUploadComplete():
+    entry_key = request.json['entryKey']
+    update_audio_file_info(entry_key)
+    response = {
+        "message": "Audio data saved.",
+    }
+    return jsonify(response)
+
 
 #returns CDN url with SAS token for a specific file
 @app.route("/api/sasToken", methods=["POST"])
@@ -328,6 +380,29 @@ def sasUrl():
     sas_url = f"{account_url}?{sas_token}"
 
     entry_key = create_entry_key(getUserID(current_token))
+    
+    # Return the SAS URL to the client
+    response = {
+        "message": "Generated SAS URL & entry key.",
+        "sasUrl": sas_url,
+        "entryKey": entry_key
+    }
+    return jsonify(response)
+
+#checks if the container exists and creates it if necessary. Then, generate a SAS token for the container and return it to the client.
+@app.route("/try/sasUrl", methods=["POST"])
+def trySasUrl():
+
+    # Call the create_container_and_generate_sas function to ensure the container exists and get the SAS token
+    sas_token = create_container_and_generate_sas('try')
+
+    # Generate the SAS URL for the container
+    connection_string = env.get("AZURE_STORAGE_CONNECTION_STRING")
+    blob_service_client = BlobServiceClient.from_connection_string(connection_string)
+    account_url = blob_service_client.primary_endpoint
+    sas_url = f"{account_url}?{sas_token}"
+
+    entry_key = create_entry_key('try')
     
     # Return the SAS URL to the client
     response = {
